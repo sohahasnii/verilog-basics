dfc4af813a15b8e602478a527bdc16e8fe58f43b
Soha Hasni <272229257+sohahasnii@users.noreply.github.com>
2026-09-15 22:56:42 +0530

    Refactor logic gates module for clarity and consistency

--- basic-logic-gates/logic_gates.v
+++ basic-logic-gates/logic_gates.v
@@ -1,11 +1,21 @@
-module logic_gates(input wire A ,
-                   input wire B,
-                   output wire OR_ , AND_ , NOT_ , NOR_ , NAND_ , XOR_ , XNOR_ );
-assign  OR_   =  A|B; 
-assign  AND_  =  A&B; 
-assign  NOT_  =  ~A;  // NOT A 
-assign  NOR_  =  ~(A|B);    
-assign  NAND_ =  ~(A&B);
-assign  XOR_  =  ((A&(~B))|((~A)&B));
-assign  XNOR_ =  ~(((A&(~B))|((~A)&B)));
+module logic_gates(
+    input wire A ,
+    input wire B ,
+    output wire OR_ ,
+    output wire AND_ ,
+    output wire NOT_ ,
+    output wire NOR_ , 
+    output wire NAND_ ,
+    output wire XOR_ ,
+    output wire XNOR_
+);
+
+assign OR_   = A | B;
+assign AND_  = A & B;
+assign NOT_  = ~A;
+assign NOR_  = ~(A | B);
+assign NAND_ = ~(A & B);
+assign XOR_  = A ^ B;
+assign XNOR_ = A ~^ B;
+
 endmodule
